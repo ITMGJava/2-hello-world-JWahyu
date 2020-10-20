fa932422f6f312049ad802d733541af5b3a898ba
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-20 23:27:35 +0200

    Opdracht 2 ter volledigheid van alle opdrachten.

---
+++ src/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println();
+
+        System.out.print("Dit is een gewone print() functie.");
+        System.out.print("Zo kan je een tekst uitprinten.");
+
+        System.out.print("Dit is een gewone print() functie." +
+                "Zo ziet hij er dan uit met een nieuwe regel");
+
+        System.out.println();
+
+        System.out.println("Dit is een println() functie.");
+        System.out.println("Dit is het verschil.");
+
+        System.out.println("Dit is een println() functie." +
+                "Met een nieuwe regel");
+
+        System.out.println();
+
+        System.out.printf("%nDit is een  printf() functie.%n" +
+                "%n%nJe kan hem aanpassen " +
+                "naar toebehoren.%n");
+
+    }
+}
